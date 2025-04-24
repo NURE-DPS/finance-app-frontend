eb9b87150490440cf4bdc9710481202e02f050eb
Mykhailo Parm <mykhailo.parm.dev@gmail.com>
2025-04-24 22:46:01 +0200

    feat: add theme

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import {Link } from "react-router-dom"
 
 export const Sidebar = () => {
   return (
-    <div className="w-64 bg-gray-900 text-white h-full p-6">
+    <div className="basis-1/7 min-h-screen h-full text-white p-6">
       <h2 className="text-xl font-bold mb-6">Finance Tracker</h2>
       <nav>
         <ul>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,9 +3,9 @@ import { Sidebar } from '../components/Sidebar'
 
 export const MainLayout = ({ children }: { children: ReactElement }) => {
   return (
-    <div className="flex h-screen bg-black">
-      <Sidebar/>
-      <div className="flex">{children}</div>
+    <div className="w-full h-full min-h-full box-border flex flex-row bg-background">
+      <Sidebar />
+      <div className="basis-3/4 flex-grow flex flex-col">{children}</div>
     </div>
   )
 }
