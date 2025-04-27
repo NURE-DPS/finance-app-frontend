ed6588ab514ae718dd384f3593ecde3cded49a4d
rrsec5 <sevcenkonik943@gmail.com>
2025-04-27 14:31:25 +0300

    refactor: add changes to pr

--- src/components/WalletCreation.tsx
+++ src/components/WalletCreation.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import { decimalInputHandler, limitedTextHandler } from '../utils/inputHandlers'
+import { LuCircleX /*LuX*/ } from 'react-icons/lu' //2 options to choose from, I prefer LuCircleX
 export default function WalletCreation({ onClose }: { onClose: () => void }) {
   const [name, setName] = useState('')
   const [currency, setCurrency] = useState('EUR')
@@ -9,59 +10,15 @@ export default function WalletCreation({ onClose }: { onClose: () => void }) {
     console.log('Wallet Added:', { name, currency, balance })
     onClose()
   }
-
-  const handleBalanceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let value = e.target.value
-
-    // Заменяем запятую на точку
-    value = value.replace(',', '.')
-
-    // Если введена только точка, делаем "0."
-    if (value === '.') {
-      value = '0.'
-    }
-
-    // Проверяем: только цифры, максимум одна точка
-    if (/^\d*\.?\d*$/.test(value)) {
-      if (value.includes('.')) {
-        // Если есть точка
-        const [, decimalPart] = value.split('.')
-        // Ограничиваем дробную часть двумя знаками
-        if (decimalPart.length > 2) {
-          return
-        }
-      }
-      //ограничиваем число 12 символами
-      if (value.length > 12) {
-        return
-      }
-      setBalance(value)
-    }
-  }
-
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-
-    // Ограничиваем длину 100 символами
-    if (value.length <= 100) {
-      setName(value)
-    }
-  }
-
-  const isBalanceEmpty = () => {
-    const numericBalance = parseFloat(balance)
-    return balance.trim() === '' || numericBalance === 0
-  }
-
   return (
-    <div className="bg-black/70 w-[600px] p-6 rounded-2xl shadow-lg mt-8">
+    <div className="bg-surface w-[600px] p-6 rounded-2xl shadow-lg mt-8">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-lg font-semibold">Add Wallet</h2>
         <button
           onClick={onClose}
-          className="text-white hover:text-gray-500 text-2xl leading-none"
+          className="text-white hover:text-gray-500 text-2xl leading-none cursor-pointer"
         >
-          &times;
+          <LuCircleX />
         </button>
       </div>
 
@@ -73,7 +30,7 @@ export default function WalletCreation({ onClose }: { onClose: () => void }) {
           <input
             type="text"
             value={name}
-            onChange={handleNameChange}
+            onChange={limitedTextHandler(setName, 100)}
             className="w-full p-2 border border-gray-300 rounded mt-1"
           />
         </div>
@@ -85,7 +42,7 @@ export default function WalletCreation({ onClose }: { onClose: () => void }) {
           <select
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded mt-1 bg-black/70" /*вот тут - я либо не ставил цвет, либо поставил такой же как в остальной части */
+            className="w-full p-2 border border-gray-300 rounded mt-1 bg-surface transition cursor-pointer"
           >
             <option value="EUR">EUR</option>
             <option value="USD">USD</option>
@@ -96,13 +53,13 @@ export default function WalletCreation({ onClose }: { onClose: () => void }) {
         <div className="mb-4">
           <label className="block text-sm font-medium text-gray-500">
             Balance{' '}
-            {isBalanceEmpty() && <span className="text-red-500">*</span>}
+            {balance.trim() === '' && <span className="text-red-500">*</span>}
           </label>
           <div className="relative">
             <input
               type="text"
               value={balance}
-              onChange={handleBalanceChange}
+              onChange={decimalInputHandler(setBalance)} // to disable removing zeros: decimalInputHandler(setBalance, false)
               className="w-full p-2 border border-gray-300 rounded mt-1 pr-16"
             />
             <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500">
@@ -115,11 +72,11 @@ export default function WalletCreation({ onClose }: { onClose: () => void }) {
           type="button"
           onClick={handleSave}
           disabled={name.trim() === '' || balance.trim() === ''}
-          className={`w-full p-2 rounded-[12px] mt-4 transition
+          className={` w-full p-2 rounded-[12px] mt-4 transition cursor-pointer
     ${
-      name.trim() === '' || isBalanceEmpty()
+      name.trim() === '' || balance.trim() === ''
         ? 'bg-gray-400 text-white cursor-not-allowed'
-        : 'bg-white text-black hover:bg-gray-300'
+        : 'bg-white text-surface hover:bg-gray-300'
     }
   `}
         >
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,25 @@
-import TopPanel from '../components/TopPanel'
+import WalletCreation from '../components/WalletCreation'
+import { useState } from 'react'
 
 export const Dashboard = () => {
+  const [isWalletCreation, setIsWalletCreation] = useState(false)
+
   return (
     <div className="text-color  h-screen">
-      <TopPanel />
+      <div className="w-full h-[60px] bg-background  px-[20px] mt-3 flex items-center justify-between">
+        <div className="text-xl font-bold">Dashboard</div>
+        <button
+          onClick={() => setIsWalletCreation(true)}
+          className="px-4 py-2 mr-4 bg-white text-black rounded-[12px] hover:bg-gray-600 transition cursor-pointer "
+        >
+          Add Wallet
+        </button>
+        {isWalletCreation && (
+          <div className="fixed inset-0 flex items-start justify-center bg-gray-300/20">
+            <WalletCreation onClose={() => setIsWalletCreation(false)} />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
