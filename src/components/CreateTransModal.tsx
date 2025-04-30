f5560d44ed256b44e54fd3d382aa52230a872b9d
rrsec5 <sevcenkonik943@gmail.com>
2025-04-30 11:50:01 +0300

    merge-components-into-dev

--- src/components/CreateTransModal.tsx
+++ src/components/CreateTransModal.tsx
@@ -1,16 +1,12 @@
-import { LuCircleX } from 'react-icons/lu'
+import { LuX } from 'react-icons/lu'
+import { IconButton } from './IconButton'
 
 export default function CreateTransModal({ onClose }: { onClose: () => void }) {
   return (
     <div className="bg-surface w-[600px] p-6 rounded-2xl shadow-lg mt-8">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-h3 text-lato font-bold mb-4">Create Trans</h2>
-        <button
-          onClick={onClose}
-          className="text-color hover:disabled text-h2 leading-none cursor-pointer"
-        >
-          <LuCircleX />
-        </button>
+        <IconButton onClick={onClose} Icon={LuX} />
       </div>
       <p>Transaction creation</p>
     </div>
