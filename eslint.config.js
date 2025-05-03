247219d092d2c54fce26cf43f7e527b641fd2af0
Mykhailo Parm <mykhailo.parm.dev@gmail.com>
2025-05-03 14:37:10 +0200

    [FTA-65] fix: failing build 2

--- eslint.config.js
+++ eslint.config.js
@@ -2,30 +2,26 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import prettier from 'eslint-plugin-prettier'
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
-import parser from '@typescript-eslint/parser'
+import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    ignores: ['dist'],
-  },
-  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:prettier/recommended',
+      'plugin:tailwindcss/recommended',
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      parser,
-      parserOptions: {
-        project: ['./tsconfig.app.json', './tsconfig.config.json'],
-      },
       ecmaVersion: 2020,
-      sourceType: 'module',
       globals: globals.browser,
     },
     plugins: {
-      '@typescript-eslint': typescriptPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      prettier,
+      prettier: 'eslint-plugin-prettier',
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -34,19 +30,6 @@ export default [
         { allowConstantExport: true },
       ],
       'prettier/prettier': 'error',
-      'no-undef': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'off',
-      '@typescript-eslint/await-thenable': 'off',
-      '@typescript-eslint/strict-boolean-expressions': 'off',
-      '@typescript-eslint/prefer-optional-chain': 'off',
-      '@typescript-eslint/prefer-nullish-coalescing': 'off',
-      '@typescript-eslint/consistent-type-imports': 'off',
-      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
-      '@typescript-eslint/no-inferrable-types': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
     },
   },
-]
+)
